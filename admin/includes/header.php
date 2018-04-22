1c8f3e4631ad1a4f3c8a3c41ef6357ed43193b8f
Mark Ian Pamintuan <ianmayfire@gmail.com>
2018-04-22 21:46:20 +0800

    Added TinyMCE WYSIWYG Editor on posts

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -42,6 +42,7 @@
     <![endif]-->
 
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
 </head>
 
 <body>

d53a3f3fc20ead9b531943d565594987ed82e882
Mark Ian Pamintuan <ianmayfire@gmail.com>
2018-05-05 23:12:26 +0800

    Refactor code and added email and username validation

--- registration.php
+++ registration.php
@@ -15,53 +15,50 @@
                         <?php
                         
                             if(isset($_POST['submit'])) {
-                                
-                                $first_name = clean($_POST['first-name']);
-                                $last_name = clean($_POST['last-name']);
-                                $username = clean($_POST['username']);
-                                $email = clean($_POST['email']);
-                                $password = clean($_POST['password']);
 
-                                if(!empty($first_name) && !empty($last_name) && !empty($username) && !empty($email) && !empty($password)) {
+                                $user_info['first_name'] = $_POST['first-name'];
+                                $user_info['last_name'] = $_POST['last-name'];
+                                $user_info['email'] = $_POST['email'];
+                                $user_info['username'] = $_POST['username'];
+                                $user_info['password'] = $_POST['password'];
 
-                                    if(userDetailDuplicate("username", $username)) {
-
+                                foreach($user_info as $info) {
+                                    if(empty($info)) {
                                         echo "<div class='alert alert-danger alert-dismissible' role='alert'>
                                         <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
-                                        Username already exists.</div>";
-
-                                    } else if(userDetailDuplicate("email", $email)) {
-
-                                        echo "<div class='alert alert-danger alert-dismissible' role='alert'>
-                                        <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
-                                        Email already exists.</div>";
-
-                                    } else {
-
-                                        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-                                        register($first_name, $last_name, $email, $username, $hashed_password);
-
-                                        $first_name = "";
-                                        $last_name = "";
-                                        $username = "";
-                                        $email = "";
-                                        $password = "";
-
-                                        echo "<div class='alert alert-success alert-dismissible' role='alert'>
-                                        <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
-                                        Your registration has been submitted.</div>";
-
+                                        Please complete all the required fields.</div>";
+                                        break;
                                     }
+                                }
 
-                                } else {
+                                if(userDetailDuplicate("username", $user_info['username'])) {
 
                                     echo "<div class='alert alert-danger alert-dismissible' role='alert'>
                                     <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
-                                    Please complete all the required fields.</div>";
+                                    Username already exists.</div>";
+
+                                } else if(userDetailDuplicate("email", $user_info['email'])) {
+
+                                    echo "<div class='alert alert-danger alert-dismissible' role='alert'>
+                                    <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+                                    Email already exists.</div>";
+
+                                } else {
+
+                                    foreach($user_info as $info) {
+                                        $info = clean($info);
+                                    }
+
+                                    $hashed_password = password_hash($user_info['password'], PASSWORD_DEFAULT);
+
+                                    register($user_info['first_name'], $user_info['last_name'], $user_info['email'], $user_info['username'], $hashed_password);
+
+                                    echo "<div class='alert alert-success alert-dismissible' role='alert'>
+                                    <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+                                    Your registration has been submitted. <a href='index.php#login_form'>Login</a></div>";
 
                                 }
-                                
+
                             }
 
                         ?>
@@ -71,23 +68,23 @@
                             <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
                                 <div class="form-group">
                                     <label for="first-name" class="sr-only">First Name</label>
-                                    <input type="text" name="first-name" id="first-name" class="form-control" placeholder="First Name" value="<?php echo isset($first_name) ? $first_name : ''; ?>" required>
+                                    <input type="text" name="first-name" id="first-name" class="form-control" placeholder="First Name" value="<?php echo isset($user_info['first_name']) ? $user_info['first_name'] : ''; ?>" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="last-name" class="sr-only">Last Name</label>
-                                    <input type="text" name="last-name" id="last-name" class="form-control" placeholder="Last Name" value="<?php echo isset($last_name) ? $last_name : ''; ?>" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="username" class="sr-only">username</label>
-                                    <input type="text" name="username" id="username" class="form-control" placeholder="Username" value="<?php echo isset($username) ? $username : ''; ?>" required>
+                                    <input type="text" name="last-name" id="last-name" class="form-control" placeholder="Last Name" value="<?php echo isset($user_info['last_name']) ? $user_info['last_name'] : ''; ?>" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="email" class="sr-only">Email</label>
-                                    <input type="email" name="email" id="email" class="form-control" placeholder="name@domain.com" value="<?php echo isset($email) ? $email : ''; ?>" required>
+                                    <input type="email" name="email" id="email" class="form-control" placeholder="name@domain.com" value="<?php echo isset($user_info['email']) ? $user_info['email'] : ''; ?>" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="username" class="sr-only">username</label>
+                                    <input type="text" name="username" id="username" class="form-control" placeholder="Username" value="<?php echo isset($user_info['username']) ? $user_info['username'] : ''; ?>" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="password" class="sr-only">Password</label>
-                                    <input type="password" name="password" id="key" class="form-control" placeholder="Password" value="<?php echo isset($password) ? $password : ''; ?>" required>
+                                    <input type="password" name="password" id="key" class="form-control" placeholder="Password" value="<?php echo isset($user_info['password']) ? $user_info['password'] : ''; ?>" required>
                                 </div>
                         
                                 <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">

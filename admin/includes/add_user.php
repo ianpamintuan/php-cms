5e7fc5195c570c79a4da2548a393be005274a00d
Mark Ian Pamintuan <ianmayfire@gmail.com>
2018-05-07 02:30:28 +0800

    Refactor users modules code

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -4,22 +4,31 @@
 
     if(isset($_POST['add_user'])) {
 
-        $user_firstname = clean($_POST['user_firstname']);
-        $user_lastname = clean($_POST['user_lastname']);
-        $user_role = clean($_POST['roles']);
-        $user_email = clean($_POST['user_email']);
-        $user_username = clean($_POST['user_username']);
-        $user_password = clean($_POST['user_password']);
+        $user_info = array();
 
-        $hashed_password = password_hash($user_password, PASSWORD_DEFAULT);
+        $user_info['first_name'] = $_POST['user_firstname'];
+        $user_info['last_name'] = $_POST['user_lastname'];
+        $user_info['user_role'] = $_POST['roles'];
+        $user_info['email'] = $_POST['user_email'];
+        $user_info['username'] = $_POST['user_username'];
+        $user_info['password'] = $_POST['user_password'];
 
-        if(userDetailDuplicate("username", $username)) {
+        foreach($user_info as $info) {
+            if(empty($info)) {
+                echo "<div class='alert alert-danger alert-dismissible' role='alert'>
+                <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+                Please complete all the required fields.</div>";
+                break;
+            }
+        }
+
+        if(userDetailDuplicate("username", $user_info['username'])) {
 
             echo "<div class='alert alert-danger alert-dismissible' role='alert'>
             <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
             Username already exists.</div>";
 
-        } else if(userDetailDuplicate("email", $email)) {
+        } else if(userDetailDuplicate("email", $user_info['email'])) {
 
             echo "<div class='alert alert-danger alert-dismissible' role='alert'>
             <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
@@ -27,18 +36,19 @@
 
         } else {
 
-            register($user_firstname, $user_lastname, $user_email, $user_username, $hashed_password, $user_role);
+            foreach($user_info as $key => $value) {
+                $user_info[$key] = clean($value);
+            }
 
-            $user_firstname = "";
-            $user_lastname = "";
-            $user_role = "";
-            $user_email = "";
-            $user_username = "";
-            $user_password = "";
+            $hashed_password = password_hash($user_info['password'], PASSWORD_DEFAULT);
+
+            register($user_info['first_name'], $user_info['last_name'], $user_info['email'], $user_info['username'], $hashed_password, $user_info['user_role']);
 
             echo "<div class='alert alert-success alert-dismissible' role='alert'>
             <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
             User added successfully. <a href='users.php'>Back to Users</a></div>";
+
+            unset($user_info);
 
         }
 
@@ -50,35 +60,46 @@
 
     <div class="form-group">
         <label for="user_firstname">First Name</label>
-        <input type="text" class="form-control" name="user_firstname" required>
+        <input type="text" class="form-control" name="user_firstname" value="<?php echo isset($user_info['first_name']) ? $user_info['first_name'] : ''; ?>" required>
     </div>
 
     <div class="form-group">
         <label for="user_lastname">Last Name</label>
-        <input type="text" class="form-control" name="user_lastname" required>
+        <input type="text" class="form-control" name="user_lastname" value="<?php echo isset($user_info['last_name']) ? $user_info['last_name'] : ''; ?>" required>
     </div>
 
     <div class="form-group">
         <label for="roles">User Role</label>
         <select name="roles" id="roles" class="form-control" required>
+            <?php 
+                if(isset($user_info['user_role'])) {
+                    echo "<option value='" . $user_info['user_role'] . "'>" .$user_info['user_role'] . "</option>";
+                    if($user_info['user_role'] !== "Admin") {
+                        echo "<option value='Admin'>Admin</option>";
+                    } else {
+                        echo "<option value='Subscriber'>Subscriber</option>";
+                    }
+                } else {
+            ?>
             <option value="Admin">Admin</option>
             <option value="Subscriber">Subscriber</option>
+            <?php   }   ?>
         </select>
     </div>
 
     <div class="form-group">
         <label for="user_email">Email</label>
-        <input type="email" class="form-control" name="user_email" required>
+        <input type="email" class="form-control" name="user_email" value="<?php echo isset($user_info['email']) ? $user_info['email'] : ''; ?>" required>
     </div>
 
     <div class="form-group">
         <label for="user_username">Username</label>
-        <input type="text" class="form-control" name="user_username" required>
+        <input type="text" class="form-control" name="user_username" value="<?php echo isset($user_info['username']) ? $user_info['username'] : ''; ?>" required>
     </div>
 
     <div class="form-group">
         <label for="user_password">Password</label>
-        <input type="password" class="form-control" name="user_password" required>
+        <input type="password" class="form-control" name="user_password" value="<?php echo isset($user_info['password']) ? $user_info['password'] : ''; ?>" required>
     </div>
 
     <div class="form-group">
--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -5,6 +5,7 @@
     if(isset($_GET['src']) == 'edit_user') {
 
         $user_id = clean($_GET['user_id']);
+        $user_info = array();
         
         $query = "SELECT * FROM tblusers WHERE user_id = {$user_id}";
         $result = mysqli_query($connection, $query);
@@ -18,12 +19,12 @@
 
         while($row = mysqli_fetch_assoc($result)) {
 
-            $user_firstname = $row['firstname'];
-            $user_lastname = $row['lastname'];
-            $user_role = $row['user_role'];
-            $user_email = $row['email'];
-            $user_username = $row['username'];
-            $user_password = $row['password'];
+            $user_info['first_name'] = $row['firstname'];
+            $user_info['last_name'] = $row['lastname'];
+            $user_info['user_role'] = $row['user_role'];
+            $user_info['email'] = $row['email'];
+            $user_info['username'] = $row['username'];
+            $user_info['password'] = $row['password'];
 
         }
 
@@ -36,20 +37,31 @@
 
     if(isset($_POST['edit_user'])) {
 
-        $user_firstname = clean($_POST['user_firstname']);
-        $user_lastname = clean($_POST['user_lastname']);
-        $user_role = clean($_POST['roles']);
-        $user_email = clean($_POST['user_email']);
-        $user_username = clean($_POST['user_username']);
-        $user_password = clean($_POST['user_password']);
+        $user_info['first_name'] = $_POST['user_firstname'];
+        $user_info['last_name'] = $_POST['user_lastname'];
+        $user_info['user_role'] = $_POST['roles'];
+        $user_info['email'] = $_POST['user_email'];
+        $user_info['username'] = $_POST['user_username'];
+        $user_info['password'] = $_POST['user_password'];
 
-        $hashed_password = password_hash($user_password, PASSWORD_DEFAULT);
+        foreach($user_info as $key => $value) {
+            $user_info[$key] = clean($value);
+        }
 
-        $query = "UPDATE tblusers SET firstname = '{$user_firstname}', lastname = '{$user_lastname}', user_role = '{$user_role}', email = '{$user_email}', username = '{$user_username}', password = '{$hashed_password}' ";
+        $hashed_password = password_hash($user_info['password'], PASSWORD_DEFAULT);
+
+        $query = "UPDATE tblusers SET firstname = '" . $user_info['first_name'] . "', lastname = '" . $user_info['last_name'] . "', user_role = '" . $user_info['user_role'] . "', email = '" . $user_info['email'] . "', username = '" . $user_info['username'] . "', password = '{$hashed_password}' ";
         $query .= "WHERE user_id = $user_id";
         $result = mysqli_query($connection, $query);
 
         checkQuery($result);
+
+        $user_info['first_name'] = $_POST['user_firstname'];
+        $user_info['last_name'] = $_POST['user_lastname'];
+        $user_info['user_role'] = $_POST['roles'];
+        $user_info['email'] = $_POST['user_email'];
+        $user_info['username'] = $_POST['user_username'];
+        $user_info['password'] = $_POST['user_password'];
 
         echo "<div class='alert alert-success alert-dismissible' role='alert'>
         <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
@@ -63,19 +75,19 @@
 
     <div class="form-group">
         <label for="user_firstname">First Name</label>
-        <input type="text" class="form-control" name="user_firstname" value="<?php echo isset($user_firstname) ? $user_firstname : ''; ?>" required>
+        <input type="text" class="form-control" name="user_firstname" value="<?php echo isset($user_info['first_name']) ? $user_info['first_name'] : ''; ?>" required>
     </div>
 
     <div class="form-group">
         <label for="user_lastname">Last Name</label>
-        <input type="text" class="form-control" name="user_lastname" value="<?php echo isset($user_lastname) ? $user_lastname : ''; ?>" required>
+        <input type="text" class="form-control" name="user_lastname" value="<?php echo isset($user_info['last_name']) ? $user_info['last_name'] : ''; ?>" required>
     </div>
     <div class="form-group">
         <label for="roles">User Role</label>
         <select name="roles" id="roles" class="form-control" required>
-            <option value='<?php echo $user_role; ?>'><?php echo $user_role; ?></option>
+            <option value="<?php echo isset($user_info['user_role']) ? $user_info['user_role'] : ''; ?>"><?php echo isset($user_info['user_role']) ? $user_info['user_role'] : ''; ?></option>
             <?php 
-                if($user_role !== 'Admin')
+                if($user_info['user_role'] !== 'Admin')
                     echo "<option value='Admin'>Admin</option>";
                 else
                     echo "<option value='Subscriber'>Subscriber</option>";
@@ -86,12 +98,12 @@
 
     <div class="form-group">
         <label for="user_email">Email</label>
-        <input type="email" class="form-control" name="user_email" value="<?php echo isset($user_email) ? $user_email : ''; ?>" required>
+        <input type="email" class="form-control" name="user_email" value="<?php echo isset($user_info['email']) ? $user_info['email'] : ''; ?>" required>
     </div>
 
     <div class="form-group">
         <label for="user_username">Username</label>
-        <input type="text" class="form-control" name="user_username" value="<?php echo isset($user_username) ? $user_username : ''; ?>" required>
+        <input type="text" class="form-control" name="user_username" value="<?php echo isset($user_info['username']) ? $user_info['username'] : ''; ?>" required>
     </div>
 
     <div class="form-group">

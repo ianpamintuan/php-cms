d55e33cb03736cee39beb922a75a7290dd711d66
Mark Ian Pamintuan <ianmayfire@gmail.com>
2018-05-07 22:08:37 +0800

    Refactor posts modules code

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -3,6 +3,7 @@
     if(isset($_GET['edit'])) {
 
         $post_id = clean($_GET['edit']);
+        $post_info = array();
 
         $query = "SELECT * FROM tblposts WHERE post_id={$post_id}";
         $result = mysqli_query($connection, $query);
@@ -16,18 +17,18 @@
 
         while($row = mysqli_fetch_assoc($result)) {
             
-            $post_id = $row['post_id'];
-            $post_title = $row['post_title'];
-            $post_content = $row['post_content'];
-            $post_author = $row['post_author'];
-            $category_id = $row['category_id'];
-            $post_status = $row['post_status'];
-            $post_image = $row['post_image'];
-            $post_tags = $row['post_tags'];
-            $post_comment_count = $row['post_comment_count'];
-            $post_views_count = $row['post_views_count'];
-            $post_date = $row['post_date'];
-        
+            $post_info['id'] = $row['post_id'];
+            $post_info['title'] = $row['post_title'];
+            $post_info['author'] = $row['post_author'];
+            $post_info['content'] = $row['post_content'];
+            $post_info['category_id'] = $row['category_id'];
+            $post_info['status'] = $row['post_status'];
+            $post_info['tags'] = $row['post_tags'];
+            $post_info['image'] = $row['post_image'];
+            $post_info['total_comments'] = $row['post_comment_count'];
+            $post_info['total_views'] = $row['post_views_count'];
+            $post_info['date'] = $row['post_date'];
+
         }
 
     } else {
@@ -36,34 +37,44 @@
     
     if(isset($_POST['update_post'])) {
 
-        $post_title = clean($_POST['post_title']);
-        $post_author = clean($_POST['post_author']);
-        $post_content = $_POST['post_content'];
-        $post_content_escaped = clean($_POST['post_content']);
-        $category_id = clean($_POST['category_id']);
-        $post_status = clean($_POST['post_status']);
-        $post_tags = clean($_POST['post_tags']);
-        
-        $post_image = $_FILES['post_image']['name'];
-        $post_image_temp = $_FILES['post_image']['tmp_name'];
+        $post_info['title'] = $_POST['post_title'];
+        $post_info['author'] = $_POST['post_author'];
+        $post_info['content'] = $_POST['post_content'];
+        $post_info['category_id'] = $_POST['category_id'];
+        $post_info['status'] = $_POST['post_status'];
+        $post_info['tags'] = $_POST['post_tags'];
+        $post_info['image'] = $_FILES['post_image']['name'];
+        $post_info['image_temp'] = $_FILES['post_image']['tmp_name'];
 
-        move_uploaded_file($post_image_temp, "../images/$post_image");
+        move_uploaded_file($post_info['image_temp'], "../images/{$post_info['image']}");
 
-        if(empty($post_image)) {
+        if(empty($post_info['image'])) {
 
-            $image_query = "SELECT * FROM tblposts WHERE post_id = {$post_id}";
+            $image_query = "SELECT * FROM tblposts WHERE post_id = {$post_info['id']}";
             $image_result = mysqli_query($connection, $image_query);
 
-            while($row = mysqli_fetch_array($image_result)) {
-                $post_image = $row['post_image']; 
-            }
+            checkQuery($image_result);
+
+            $row = mysqli_fetch_array($image_result);
+            $post_info['image'] = $row['post_image']; 
         
         }
 
-        $query = "UPDATE tblposts SET post_title = '{$post_title}', post_author = '{$post_author}', category_id = {$category_id}, post_content = '{$post_content_escaped}', post_status = '{$post_status}', post_tags = '{$post_tags}', post_image = '{$post_image}' WHERE post_id = {$post_id}";
+        foreach($post_info as $key => $value) {
+            $post_info[$key] = clean($value);
+        }
+
+        $query = "UPDATE tblposts SET post_title = '" . $post_info['title'] . "', post_author = '" . $post_info['author'] . "', category_id = " . $post_info['category_id'] . ", post_content = '" . $post_info['content'] . "', post_status = '" . $post_info['status'] . "', post_tags = '" . $post_info['status'] . "', post_image = '" . $post_info['image'] . "' WHERE post_id = " . $post_info['id'];
         $result = mysqli_query($connection, $query);
 
         checkQuery($result);
+
+        $post_info['title'] = $_POST['post_title'];
+        $post_info['author'] = $_POST['post_author'];
+        $post_info['content'] = $_POST['post_content'];
+        $post_info['category_id'] = $_POST['category_id'];
+        $post_info['status'] = $_POST['post_status'];
+        $post_info['tags'] = $_POST['post_tags'];
 
         echo "<div class='alert alert-success alert-dismissible' role='alert'>
         <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
@@ -77,25 +88,25 @@
 
     <div class="form-group">
         <label for="title">Post Title</label>
-        <input type="text" class="form-control" name="post_title" value="<?php echo $post_title; ?>" required>
+        <input type="text" class="form-control" name="post_title" value="<?php echo $post_info['title']; ?>" required>
     </div>
 
     <div class="form-group">
         <label for="category_id">Post Category</label>
-        <?php displayCategories("dropdown", $category_id); ?>
+        <?php displayCategories("dropdown", $post_info['category_id']); ?>
     </div>
 
     <div class="form-group">
         <label for="post_author">Post Author</label>
-        <?php displayUsers("dropdown", $post_author); ?>
+        <?php displayUsers("dropdown", $post_info['author']); ?>
     </div>
 
     <div class="form-group">
         <label for="post_status">Post Status</label>
         <select name="post_status" id="post_status" class="form-control">
-            <option value="<?php echo $post_status; ?>"><?php echo $post_status; ?></option>
+            <option value="<?php echo $post_info['status']; ?>"><?php echo $post_info['status']; ?></option>
             <?php
-                if($post_status !== "Draft")
+                if($post_info['status'] !== "Draft")
                     echo "<option value='Draft'>Draft</option>";
                 else
                     echo "<option value='Published'>Published</option>";
@@ -105,18 +116,18 @@
     
     <div class="form-group">
         <label for="post_image">Post Image</label>
-        <img width="100" src="../images/<?php echo $post_image; ?>" alt="Post image" class="img-responsive" style="margin-bottom: 10px;">
+        <img width="100" src="../images/<?php echo $post_info['image']; ?>" alt="Post image" class="img-responsive" style="margin-bottom: 10px;">
         <input type="file" class="form-control" name="post_image" accept="image/*">
     </div>
 
     <div class="form-group">
         <label for="post_tags">Post Tags</label>
-        <input type="text" class="form-control" name="post_tags" value="<?php echo $post_tags; ?>">
+        <input type="text" class="form-control" name="post_tags" value="<?php echo $post_info['tags']; ?>">
     </div>
 
     <div class="form-group">
         <label for="post_content">Post Content</label>
-        <textarea name="post_content" class="form-control" cols="30" rows="10"><?php echo $post_content; ?></textarea>
+        <textarea name="post_content" class="form-control" cols="30" rows="10"><?php echo $post_info['content']; ?></textarea>
     </div>
 
     <div class="form-group">

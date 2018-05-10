a227a5f3833a334a171ca08a3a6b19cee6f269b9
Mark Ian Pamintuan <ianmayfire@gmail.com>
2018-05-10 23:08:35 +0800

    Updated SQL file

--- db/dbcms.sql
+++ db/dbcms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 02, 2018 at 09:09 PM
+-- Generation Time: May 10, 2018 at 05:07 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -52,7 +52,7 @@ CREATE TABLE `tblcomments` (
   `comment_author` varchar(255) NOT NULL,
   `comment_email` varchar(255) NOT NULL,
   `comment_content` text NOT NULL,
-  `comment_status` varchar(255) NOT NULL,
+  `comment_status` varchar(255) NOT NULL DEFAULT 'Unapproved',
   `comment_date` date NOT NULL,
   `post_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -80,9 +80,9 @@ CREATE TABLE `tblposts` (
   `post_image` text NOT NULL,
   `post_content` text NOT NULL,
   `post_tags` varchar(255) NOT NULL,
-  `post_comment_count` int(11) NOT NULL,
-  `post_status` varchar(255) NOT NULL DEFAULT 'draft',
-  `post_views_count` int(11) NOT NULL,
+  `post_comment_count` int(11) NOT NULL DEFAULT '0',
+  `post_status` varchar(255) NOT NULL,
+  `post_views_count` int(11) NOT NULL DEFAULT '0',
   `category_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -91,13 +91,10 @@ CREATE TABLE `tblposts` (
 --
 
 INSERT INTO `tblposts` (`post_id`, `post_title`, `post_author`, `post_date`, `post_image`, `post_content`, `post_tags`, `post_comment_count`, `post_status`, `post_views_count`, `category_id`) VALUES
-(1, 'PHP is awesome!', 1, '2017-08-19', 'main-image-min.jpeg', '<!DOCTYPE html>\r\n<html>\r\n<head>\r\n</head>\r\n<body>\r\n<p>This is a really a powerful language to learn in web development!</p>\r\n</body>\r\n</html>', 'PHP, JS', 2, 'Published', 28, 3),
-(4, 'Javascript is the future', 1, '2017-04-24', 'ARCHITECTS-All-Our-Gods-Have-Abandoned-Us-620x330.jpg', '<!DOCTYPE html>\r\n<html>\r\n<head>\r\n</head>\r\n<body>\r\n<p>Javascript is progressing rapidly. At first it is only used on front-end development, but now it can now be used as a back-end language for web and mobile development.</p>\r\n</body>\r\n</html>', 'Javascript', 1, 'Published', 3, 2),
+(1, 'PHP is awesome!', 1, '2017-08-19', 'main-image-min.jpeg', '<!DOCTYPE html>\r\n<html>\r\n<head>\r\n</head>\r\n<body>\r\n<p>This is a really a powerful language to learn in web development!</p>\r\n</body>\r\n</html>', 'PHP, JS', 3, 'Published', 53, 3),
+(4, 'Javascript is the future', 1, '2017-04-24', 'ARCHITECTS-All-Our-Gods-Have-Abandoned-Us-620x330.jpg', '<!DOCTYPE html>\r\n<html>\r\n<head>\r\n</head>\r\n<body>\r\n<p>Javascript is progressing rapidly. At first it is only used on front-end development, but now it can now be used as a back-end language for web and mobile development.</p>\r\n</body>\r\n</html>', 'Javascript', 1, 'Published', 6, 2),
 (6, 'Another PHP post', 1, '2017-09-17', 'oil.jpg', '<!DOCTYPE html>\n<html>\n<head>\n</head>\n<body>\n<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\n</body>\n</html>', 'PHP', 0, 'Published', 0, 3),
-(8, 'PHP is awesome!', 1, '2017-08-19', 'main-image-min.jpeg', '<!DOCTYPE html>\r\n<html>\r\n<head>\r\n</head>\r\n<body>\r\n<p>This is a really a powerful language to learn in web development!</p>\r\n</body>\r\n</html>', 'PHP, JS', 0, 'Published', 0, 3),
-(9, 'Javascript is the future', 1, '2017-04-24', 'ARCHITECTS-All-Our-Gods-Have-Abandoned-Us-620x330.jpg', '<!DOCTYPE html>\r\n<html>\r\n<head>\r\n</head>\r\n<body>\r\n<p>Javascript is progressing rapidly. At first it is only used on front-end development, but now it can now be used as a back-end language for web and mobile development.</p>\r\n</body>\r\n</html>', 'Javascript', 0, 'Published', 2, 2),
-(10, 'Another PHP post', 1, '2017-09-17', 'oil.jpg', '<!DOCTYPE html>\n<html>\n<head>\n</head>\n<body>\n<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\n</body>\n</html>', 'PHP', 0, 'Published', 2, 3),
-(11, 'Test', 12, '2018-05-03', '', '<!DOCTYPE html>\r\n<html>\r\n<head>\r\n</head>\r\n<body>\r\n<p>Test</p>\r\n</body>\r\n</html>', 'Test', 0, 'Published', 5, 1);
+(12, 'Yay', 1, '0000-00-00', '', '<p>yay</p>', 'html', 0, 'Draft', 1, 2018);
 
 -- --------------------------------------------------------
 
@@ -116,9 +113,8 @@ CREATE TABLE `tblsessions` (
 --
 
 INSERT INTO `tblsessions` (`id`, `session`, `time`) VALUES
-(1, 'tilaerqmu7jd643koakgogbce6', 1525288164),
-(2, 'm1550rani278skmekd4b3acg73', 1525190847),
-(3, '', 1525109117);
+(158, 'm1550rani278skmekd4b3acg73', 1525964811),
+(159, 'tilaerqmu7jd643koakgogbce6', 1525789077);
 
 -- --------------------------------------------------------
 
@@ -134,17 +130,20 @@ CREATE TABLE `tblusers` (
   `lastname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `image` text NOT NULL,
-  `user_role` varchar(255) NOT NULL DEFAULT 'Subscriber'
+  `user_role` varchar(255) NOT NULL DEFAULT 'Subscriber',
+  `token` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblusers`
 --
 
-INSERT INTO `tblusers` (`user_id`, `username`, `password`, `firstname`, `lastname`, `email`, `image`, `user_role`) VALUES
-(1, 'admin', '$2y$10$yCFtRW5W4el3CvrzdMiKse8z7vOPLQg1KGGYzZMjtiZowisBjTVoG', 'Mark Ian', 'Pamintuan', 'markian@php-cms.com', '', 'Admin'),
-(12, 'hikki', '$2y$10$8zurIcBwFisr.7qf9C07PeI7s7R8E...70eCe31AoPgW6nBuLz8zK', 'Hikigaya', 'Hachiman', 'hikki@gmail.com', '', 'Subscriber'),
-(14, 'test', '$2y$10$Cgh25VCob7VDQnLnM.lx6.LHjJBH2lJMa1znK5wddOr4YR9T15nE2', 'Test', 'Test', 'test@test.com', '', 'Subscriber');
+INSERT INTO `tblusers` (`user_id`, `username`, `password`, `firstname`, `lastname`, `email`, `image`, `user_role`, `token`) VALUES
+(1, 'admin', '$2y$10$N5to9I//wfK5DVbz0Cxdjei3srhVzk/poo5VlyzSLhqN2r1AHAc9i', 'Mark Ian', 'Pamintuan', 'markian@php-cms.com', '', 'Admin', ''),
+(12, 'hikki', '$2y$10$IybETSkwarB2jXAQMyo4SeYuYhA0n2HyOzLwf.qtoPKv48.NqJoYq', 'Hikigaya', 'Hachiman', 'hikki@gmail.com', '', 'Subscriber', ''),
+(14, 'test', '$2y$10$Cgh25VCob7VDQnLnM.lx6.LHjJBH2lJMa1znK5wddOr4YR9T15nE2', 'Test', 'Test', 'test@test.com', '', 'Subscriber', ''),
+(18, 'deku', '$2y$10$Oq9B4fPkl4lk4981jxlMA.KU.bQiHf6PBJQ5XOllKKGUW5tHRhf6u', 'Deku', 'Midoriya', 'deku@hero.com', '', 'Subscriber', ''),
+(26, 'sam', '$2y$10$GNW62vdVJIBY9jbKX7DE0O3/VTB0YfnT8CU16HRSyMIbRQO8ZL5qW', 'sam', 'sam', 'sam@sam.com', '', 'Subscriber', '');
 
 --
 -- Indexes for dumped tables
@@ -154,31 +153,41 @@ INSERT INTO `tblusers` (`user_id`, `username`, `password`, `firstname`, `lastnam
 -- Indexes for table `tblcategories`
 --
 ALTER TABLE `tblcategories`
-  ADD PRIMARY KEY (`category_id`);
+  ADD PRIMARY KEY (`category_id`),
+  ADD KEY `category_id` (`category_id`);
 
 --
 -- Indexes for table `tblcomments`
 --
 ALTER TABLE `tblcomments`
-  ADD PRIMARY KEY (`comment_id`);
+  ADD PRIMARY KEY (`comment_id`),
+  ADD KEY `comment_id` (`comment_id`),
+  ADD KEY `post_id` (`post_id`);
 
 --
 -- Indexes for table `tblposts`
 --
 ALTER TABLE `tblposts`
-  ADD PRIMARY KEY (`post_id`);
+  ADD PRIMARY KEY (`post_id`),
+  ADD KEY `post_id` (`post_id`),
+  ADD KEY `category_id` (`category_id`);
 
 --
 -- Indexes for table `tblsessions`
 --
 ALTER TABLE `tblsessions`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
 
 --
 -- Indexes for table `tblusers`
 --
 ALTER TABLE `tblusers`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `username` (`username`),
+  ADD KEY `email` (`email`),
+  ADD KEY `user_role` (`user_role`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -188,27 +197,27 @@ ALTER TABLE `tblusers`
 -- AUTO_INCREMENT for table `tblcategories`
 --
 ALTER TABLE `tblcategories`
-  MODIFY `category_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `category_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 --
 -- AUTO_INCREMENT for table `tblcomments`
 --
 ALTER TABLE `tblcomments`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `tblposts`
 --
 ALTER TABLE `tblposts`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
 --
 -- AUTO_INCREMENT for table `tblsessions`
 --
 ALTER TABLE `tblsessions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=160;
 --
 -- AUTO_INCREMENT for table `tblusers`
 --
 ALTER TABLE `tblusers`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
